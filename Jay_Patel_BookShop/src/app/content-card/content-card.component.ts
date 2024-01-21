664bf6956589f12cee5ccf419f9c1c1a68280fa2
JayPatel202002 <125040098+JayPatel20200@users.noreply.github.com>
2024-01-21 16:58:00 -0500

    Added the class and content in it.

---
+++ Jay_Patel_BookShop/src/app/content-card/content-card.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { ContentList } from '../helper-files/content-list';
+
+@Component({
+  selector: 'app-content-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './content-card.component.html',
+  styleUrl: './content-card.component.scss'
+})
+export class ContentCardComponent {
+  contentList: ContentList = new ContentList();
+
+  constructor(){
+    this.contentList.addContent({
+      id: 0,
+      title: "The Alchemist",
+      description:"Quest for personal legend unfolds.",
+      creator:"Paulo Coelho",
+      imgURL:"app/content-card/photo/",
+      type:"Fiction"
+    });
+    this.contentList.addContent({
+      id: 1,
+      title: "1984",
+      description:"Dystopian surveillance challenges individual freedom.",
+      creator:"George Orwell",
+      imgURL:"app/content-card/photo/",
+      type:"Dystopian Fiction"
+    });
+    this.contentList.addContent({
+      id: 2,
+      title: "To Kill a Mockingbird",
+      description:"Southern racial injustice witnessed by young Scout.",
+      creator:"Harper Lee",
+      imgURL:"app/content-card/photo/",
+      type:"Classic"
+    });
+  }
+}
