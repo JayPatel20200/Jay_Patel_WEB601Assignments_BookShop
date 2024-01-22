2bb93bdae8bb96f9a2f91ed3cdd9985de717207e
JayPatel202002 <125040098+JayPatel20200@users.noreply.github.com>
2024-01-21 20:50:03 -0500

    did some final update in app html

--- Jay_Patel_BookShop/src/app/app.component.ts
+++ Jay_Patel_BookShop/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ContentList } from './helper-files/content-list';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,34 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Jay_Patel_BookShop';
+    title = 'Jay_Patel_BookShop';
+    contentList: ContentList = new ContentList();
+
+    constructor(){
+      this.contentList.addContent({
+        id: 0,
+        title: "The Alchemist",
+        description:"Quest for personal legend unfolds.",
+        creator:"Paulo Coelho",
+        imgURL:"app/content-card/photo/",
+        type:"Fiction"
+      });
+      this.contentList.addContent({
+        id: 1,
+        title: "1984",
+        description:"Dystopian surveillance challenges individual freedom.",
+        creator:"George Orwell",
+        imgURL:"app/content-card/photo/",
+        type:"Dystopian Fiction"
+      });
+      this.contentList.addContent({
+        id: 2,
+        title: "To Kill a Mockingbird",
+        description:"Southern racial injustice witnessed by young Scout.",
+        creator:"Harper Lee",
+        imgURL:"app/content-card/photo/",
+        type:"Classic"
+      });
+      this.contentList.getHtmlOutputAtIndex(2)
+  }
 }
