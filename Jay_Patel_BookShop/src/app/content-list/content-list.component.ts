86bacbe6d7f9bf813d4cdecd157d4f2346dbe45c
JayPatel202002 <125040098+JayPatel20200@users.noreply.github.com>
2024-02-06 16:15:43 -0500

    content is displaying and search is also working

--- Jay_Patel_BookShop/src/app/content-list/content-list.component.ts
+++ Jay_Patel_BookShop/src/app/content-list/content-list.component.ts
@@ -1,19 +1,33 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { Content } from '../helper-files/content-interface';
-
+import { ContentCardComponent } from '../content-card/content-card.component';
+import { TypeFilterPipe } from '../type-filter.pipe';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-content-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ContentCardComponent, TypeFilterPipe, FormsModule],
   templateUrl: './content-list.component.html',
   styleUrl: './content-list.component.scss'
 })
 export class ContentListComponent implements OnInit {
-  displayContentInfo(contentItem: Content) {
+  DisplayContentInformation(contentItem: Content) {
     console.log(`ID: ${contentItem.id} and Title: ${contentItem.title}`);
     }
   @Input () contentItems: Content[] = [];
+
+  searchTitle: string = '';
+  contentExists: boolean = false;
+  message: string = '';  
+  selectedTitle: string | null = null;
+
+  checkContentExists() {
+    const foundItem = this.contentItems.find(item => item.title.toLowerCase() === this.searchTitle.toLowerCase());
+    this.contentExists = !!foundItem;
+    this.message = foundItem ? 'Content item exists.' : 'Content item does not exist.';
+    this.selectedTitle = foundItem ? foundItem.title : null;
+  }
 
   ngOnInit(): void {
     this.contentItems =[
@@ -32,7 +46,7 @@ export class ContentListComponent implements OnInit {
         description: "A novel by Harper Lee",
         creator: "Harper Lee",
         imgURL: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/To_Kill_a_Mockingbird_%281963_US_theatrical_poster%29.jpg/800px-To_Kill_a_Mockingbird_%281963_US_theatrical_poster%29.jpg",
-        type: "Novel",
+        type: "Friction",
         tags: ["Fiction", "Classic"]
       },
       {
@@ -41,7 +55,7 @@ export class ContentListComponent implements OnInit {
         description: "https://i.ebayimg.com/images/g/u6wAAOSwwX1hN7xf/s-l1200.webp",
         creator: "George Orwell",
         imgURL: "https://res.cloudinary.com/jerrick/image/upload/v1678620098/640db5c1a93664001d8e2953.jpg",
-        type: "Novel",
+        type: "AutoBiography",
         tags: ["Fiction", "Dystopian"]
       },
       {
@@ -59,7 +73,7 @@ export class ContentListComponent implements OnInit {
         description: "A novel by J.D. Salinger",
         creator: "J.D. Salinger",
         imgURL: "https://media.npr.org/assets/artslife/books/2009/10/catcher_custom-853c2f7a4f9f9acaa8647dfdc7b9796555ad54a2-s1100-c50.jpg",
-        type: "Novel",
+        type: "Friction",
         tags: ["Fiction", "Coming-of-age"]
       },
       {
@@ -68,7 +82,7 @@ export class ContentListComponent implements OnInit {
         description: "A novel by Harper Lee",
         creator: "Harper Lee",
         imgURL: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/To_Kill_a_Mockingbird_%281963_US_theatrical_poster%29.jpg/800px-To_Kill_a_Mockingbird_%281963_US_theatrical_poster%29.jpg",
-        type: "Novel",
+        type: "AutoBiography",
         tags: ["Fiction", "Classic"]
       }
       
